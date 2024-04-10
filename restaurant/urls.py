22181bc0fdc3289769cd62bde0f9ed93818304ed
JoshuaGlaZ <joshuaminex02@gmail.com>
2024-04-10 18:27:07 +0700

    Add static home page path

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
   path('', views.index, name='index'),
   path('menu', views.MenuItemsView.as_view()),
   path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
-  path('api-token-auth/', obtain_auth_token),
+  path('api-token-auth', obtain_auth_token),
 ]
